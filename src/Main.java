6af0095e2cd9095fe06e3571d161dfaf975da034
Matias D. Scoponi <101079021+matii1290@users.noreply.github.com>
2022-11-25 15:33:56 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-    Persona persona = new Persona();
+        Persona persona = new Persona();
 
-    persona.setNombre("Matias");
-    persona.setEdad(26);
-    persona.setTelefono(15405500);
+        persona.setNombre("Matias");
+        persona.setEdad(26);
+        persona.setTelefono(15405500);
 
-    System.out.println(persona.nombre);
-    System.out.println(persona.edad);
-    System.out.println(persona.telefono);
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getTelefono());
     }
 }
 
@@ -18,28 +18,23 @@ class Persona {
     private int Telefono;
 
 
-    String nombre = Nombre;
-    Integer edad = Edad;
-    Integer telefono = Telefono;
-
-
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.Nombre = nombre;
     }
     public void setEdad(int edad) {
-        this.edad = edad;
+        this.Edad = edad;
     }
     public void setTelefono(int telefono) {
-        this.telefono = telefono;
+        this.Telefono = telefono;
     }
 
     public String getNombre() {
         return this.Nombre;
     }
     public int getEdad() {
-        return this.edad;
+        return this.Edad;
     }
     public int getTelefono() {
-        return this.telefono;
+        return this.Telefono;
     }
 }
